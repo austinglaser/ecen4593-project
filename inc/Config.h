fca7437083fd3a7059d4e1b039c364fa99549451
Austin Glaser <austin@boulderes.com>
2016-03-16 13:40:51 -0600

    skeleton of config reader

---
+++ inc/Config.h
@@ -0,0 +1,33 @@
+/**
+ * @file    Config.h
+ * @author  Austin Glaser <austin@boulderes.com>
+ * @brief   Config Interface
+ */
+
+#ifndef CONFIG_H
+#define CONFIG_H
+
+/**@defgroup CONFIG Config
+ * @{ 
+ *
+ * @brief
+ */
+
+/* --- PUBLIC DEPENDENCIES -------------------------------------------------- */
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* --- PUBLIC CONSTANTS ----------------------------------------------------- */
+/* --- PUBLIC DATATYPES ----------------------------------------------------- */
+
+typedef struct config {
+} config_t;
+
+/* --- PUBLIC MACROS -------------------------------------------------------- */
+/* --- PUBLIC VARIABLES ----------------------------------------------------- */
+/* --- PUBLIC FUNCTIONS ----------------------------------------------------- */
+
+/** @} defgroup CONFIG */
+
+#endif /* ifndef CONFIG_H */
---
+++ test/test_Config.c
@@ -0,0 +1,37 @@
+/**
+ * @file    test_Config.c
+ * @author  Austin Glaser <austin@boulderes.com>
+ * @brief   Config Test
+ *
+ * @addtogroup TEST_CONFIG
+ * @{
+ */
+
+/* --- PRIVATE DEPENDENCIES ------------------------------------------------- */
+
+#include "unity.h"
+
+#include "Config.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* --- PRIVATE CONSTANTS ---------------------------------------------------- */
+/* --- PRIVATE DATATYPES ---------------------------------------------------- */
+/* --- PRIVATE MACROS ------------------------------------------------------- */
+/* --- PRIVATE FUNCTION PROTOTYPES ------------------------------------------ */
+/* --- PUBLIC VARIABLES ----------------------------------------------------- */
+/* --- PRIVATE VARIABLES ---------------------------------------------------- */
+/* --- PUBLIC FUNCTIONS ----------------------------------------------------- */
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+/* --- PRIVATE FUNCTION DEFINITIONS ----------------------------------------- */
+
+/** @} addtogroup TEST_CONFIG */

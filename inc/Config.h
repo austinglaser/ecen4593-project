95dfe4924b4c543847c1131bdc924bc7d430ea47
Austin Glaser <austin@boulderes.com>
2016-03-20 23:40:47 -0600

    add a comment (whitespace commit)

--- inc/Config.h
+++ inc/Config.h
@@ -57,6 +57,10 @@ void Config_Defaults(config_t * configp);
 /**@brief   Parses the given line, and modifies the appropriate value in configp */
 void Config_ParseLine(const char * line, config_t * configp);
 
+/**@brief   Opens the file, and reads in all the config lines.
+ *
+ * If a value isn't specified, uses the default configuration value
+ */
 void Config_FromFile(const char * filename, config_t * configp);
 
 /** @} defgroup CONFIG */

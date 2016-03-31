39cb27f48a0cf304c81349892d6e935e4fddd498
Austin Glaser <austin@boulderes.com>
2016-03-31 00:51:06 -0600

    add some fully-associative cache data tests. could be expanded

---
+++ test/test_CacheData_FullyAssociative.c
@@ -0,0 +1,118 @@
+/**
+ * @file    test_CacheData_FullyAssociative.c
+ * @author  Austin Glaser <austin@boulderes.com>
+ * @brief   TestCacheDataFullyAssociative Source
+ *
+ * @addtogroup TEST_CACHEDATA_FULLYASSOCIATIVE
+ * @{
+ */
+
+/* --- PRIVATE DEPENDENCIES ------------------------------------------------- */
+
+#include "CacheData.h"
+#include "unity.h"
+
+#include "Util.h"
+
+#include "CException.h"
+#include "CExceptionConfig.h"
+#include "ExceptionTypes.h"
+
+#include <stdbool.h>
+#include <stdint.h>
+
+/* --- PRIVATE CONSTANTS ---------------------------------------------------- */
+/* --- PRIVATE DATATYPES ---------------------------------------------------- */
+/* --- PRIVATE MACROS ------------------------------------------------------- */
+/* --- PRIVATE FUNCTION PROTOTYPES ------------------------------------------ */
+/* --- PUBLIC VARIABLES ----------------------------------------------------- */
+/* --- PRIVATE VARIABLES ---------------------------------------------------- */
+
+static const uint32_t n_sets           = 1;
+static const uint32_t set_len          = 8;
+static const uint32_t block_size_bytes = 4;
+static cache_data_t cache_data;
+
+/* --- PUBLIC FUNCTIONS ----------------------------------------------------- */
+
+void setUp(void)
+{
+    cache_data = CacheData_Create(n_sets, set_len, block_size_bytes);
+}
+
+void tearDown(void)
+{
+    CacheData_Destroy(cache_data);
+}
+
+void test_CacheData_Read_should_StoreAllBlocks(void)
+{
+    uint64_t base_address = 0x71234200;
+
+    uint32_t i;
+    for (i = 0; i < set_len; i++) {
+        uint64_t address = base_address + i * block_size_bytes;
+        CacheData_Read(cache_data, address);
+    }
+
+    for (i = 0; i < set_len; i++) {
+        uint64_t address = base_address + i * block_size_bytes;
+        TEST_ASSERT(CacheData_Contains(cache_data, address));
+    }
+}
+
+void test_CacheData_Read_should_KickoutOldestBlock(void)
+{
+    uint64_t base_address = 0x71234200;
+
+    uint32_t i;
+    for (i = 0; i < set_len + 1; i++) {
+        uint64_t address = base_address + i * block_size_bytes;
+        CacheData_Read(cache_data, address);
+    }
+
+    TEST_ASSERT_FALSE(CacheData_Contains(cache_data, base_address));
+
+    for (i = 1; i < set_len + 1; i++) {
+        uint64_t address = base_address + i * block_size_bytes;
+        TEST_ASSERT(CacheData_Contains(cache_data, address));
+    }
+}
+
+void test_CacheData_Read_should_KickoutOldestBlockDirty_when_BlockHasBeenWritten(void)
+{
+    uint64_t base_address = 0x71234200;
+
+    CacheData_Write(cache_data, base_address);
+
+    uint32_t i;
+    for (i = 1; i < set_len; i++) {
+        uint64_t address = base_address + i * block_size_bytes;
+        CacheData_Read(cache_data, address);
+    }
+
+    TEST_ASSERT_EQUAL_HEX64(base_address, CacheData_Read(cache_data, base_address + set_len * block_size_bytes));
+}
+
+void test_CacheData_Write_should_BringBlockToFrontOfLRU(void)
+{
+    uint64_t base_address = 0x71234200;
+
+    uint32_t i;
+    for (i = 0; i < set_len; i++) {
+        uint64_t address = base_address + i * block_size_bytes;
+        CacheData_Read(cache_data, address);
+    }
+
+    CacheData_Write(cache_data, base_address);
+
+    for (i = 0; i < set_len - 1; i++) {
+        uint64_t address = base_address + (i + set_len) * block_size_bytes;
+        CacheData_Read(cache_data, address);
+    }
+    TEST_ASSERT_EQUAL_HEX(base_address, CacheData_Read(cache_data, base_address + 2 * set_len * i * block_size_bytes));
+}
+
+/* --- PRIVATE FUNCTION DEFINITIONS ----------------------------------------- */
+
+/** @} addtogroup TEST_CACHEDATA_FULLYASSOCIATIVE */

ecbc0ac52dc5b8b6f229ba8d4959b94d20768c46
Austin Glaser <austin@boulderes.com>
2016-04-05 00:14:01 -0600

    main grabs its config file

--- src/main.c
+++ src/main.c
@@ -9,10 +9,12 @@
 
 /* --- PRIVATE DEPENDENCIES ------------------------------------------------- */
 
-#include "CException.h"
+#include "Config.h"
 
+#include <inttypes.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <stdio.h>
 
 /* --- PRIVATE DATATYPES ---------------------------------------------------- */
 /* --- PRIVATE FUNCTION PROTOTYPES ------------------------------------------ */
@@ -25,8 +27,23 @@
 /**
  * @brief   Application Entry Point
  */
-int main(void)
+int main(int argc, char const * const * const argv)
 {
+    if (argc > 2) {
+        printf("Usage: %s [config_file]\n", argv[0]);
+        return -1;
+    }
+
+    char const * config_file = NULL;
+    if (argc == 2) {
+        config_file = argv[1];
+    }
+
+    config_t config;
+    Config_FromFile(config_file, &config);
+
+    Config_Print(&config);
+
     return 0;
 }
 

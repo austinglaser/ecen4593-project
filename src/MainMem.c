2957e5d1abc4ebe776989fed2472fe077b854f00
Austin Glaser <austin@boulderes.com>
2016-04-06 20:00:24 -0600

    main-mem access doesnt need to be chunk aligned?

--- src/MainMem.c
+++ src/MainMem.c
@@ -14,6 +14,7 @@
 #include "Config.h"
 #include "Util.h"
 
+#include <assert.h>
 #include <inttypes.h>
 #include <stdbool.h>
 #include <stdint.h>
@@ -54,11 +55,10 @@ uint32_t MainMem_Access(main_mem_t mem, access_t const * access)
     uint32_t access_cycles = mem->config->send_address_cycles +
                              mem->config->ready_cycles;
 
-    access_t chunk_aligned_access;
-    Access_Align(&chunk_aligned_access, access, mem->config->chunk_size_bytes);
-    uint32_t n_chunks = chunk_aligned_access.n_bytes / mem->config->chunk_size_bytes;
+    uint32_t n_chunks = access->n_bytes / mem->config->chunk_size_bytes;
 
     access_cycles += n_chunks * mem->config->send_chunk_cycles;
+
 
     return access_cycles;
 }
--- test/test_MainMem.c
+++ test/test_MainMem.c
@@ -68,44 +68,6 @@ void test_AlignedBusWidthAccess(void)
     TEST_ASSERT_EQUAL_UINT32(expected_access_cycles, MainMem_Access(main_mem, &access));
 }
 
-void test_AlignedSmallerThanBusAccess(void)
-{
-    access_t access = {
-        .type       = TYPE_READ,
-        .address    = 0x08004580,
-        .n_bytes    = 4,
-    };
-
-    TEST_ASSERT_MESSAGE( (access.address % config.main_mem.chunk_size_bytes == 0) &&
-                         (access.n_bytes < config.main_mem.chunk_size_bytes),
-                         "Test parameters need attention");
-
-    uint32_t expected_access_cycles = config.main_mem.send_address_cycles +
-                                      config.main_mem.ready_cycles +
-                                      config.main_mem.send_chunk_cycles;
-
-    TEST_ASSERT_EQUAL_UINT32(expected_access_cycles, MainMem_Access(main_mem, &access));
-}
-
-void test_UnalignedBusWidthAccess(void)
-{
-    access_t access = {
-        .type       = TYPE_INSTR,
-        .address    = 0x08004584,
-        .n_bytes    = 8,
-    };
-
-    TEST_ASSERT_MESSAGE( (access.address % config.main_mem.chunk_size_bytes != 0) &&
-                         (access.n_bytes == config.main_mem.chunk_size_bytes),
-                         "Test parameters need attention");
-
-    uint32_t expected_access_cycles = config.main_mem.send_address_cycles +
-                                      config.main_mem.ready_cycles +
-                                      (2 * config.main_mem.send_chunk_cycles);
-
-    TEST_ASSERT_EQUAL_UINT32(expected_access_cycles, MainMem_Access(main_mem, &access));
-}
-
 void test_LargeAccess(void)
 {
     access_t access = {
@@ -121,25 +83,6 @@ void test_LargeAccess(void)
     uint32_t expected_access_cycles = config.main_mem.send_address_cycles +
                                       config.main_mem.ready_cycles +
                                       (8 * config.main_mem.send_chunk_cycles);
-
-    TEST_ASSERT_EQUAL_UINT32(expected_access_cycles, MainMem_Access(main_mem, &access));
-}
-
-void test_LargeUnalignedAccess(void)
-{
-    access_t access = {
-        .type       = TYPE_INSTR,
-        .address    = 0x08004581,
-        .n_bytes    = 64,
-    };
-
-    TEST_ASSERT_MESSAGE( (access.address % config.main_mem.chunk_size_bytes != 0) &&
-                         (access.n_bytes > config.main_mem.chunk_size_bytes),
-                         "Test parameters need attention");
-
-    uint32_t expected_access_cycles = config.main_mem.send_address_cycles +
-                                      config.main_mem.ready_cycles +
-                                      (9 * config.main_mem.send_chunk_cycles);
 
     TEST_ASSERT_EQUAL_UINT32(expected_access_cycles, MainMem_Access(main_mem, &access));
 }

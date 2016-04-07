115f1e7f05fa6084f6d582b84731504779b7c068
Austin Glaser <austin@boulderes.com>
2016-04-06 22:48:51 -0600

    add script for running all tests with sample output

---
+++ support/verify.py
@@ -0,0 +1,54 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+#
+# Copyright © 2016 Austin Glaser <austin@boulderes.com>
+#
+# Distributed under terms of the MIT license.
+
+"""
+Verifies all results and configurations
+"""
+
+import os
+import subprocess
+
+
+if __name__ == "__main__":
+    config_dir = 'config'
+    traces_dir = 'traces'
+    results_dir = 'results'
+    traces_short_dir = os.path.join(traces_dir, 'traces-short')
+    traces_5M_dir = os.path.join(traces_dir, 'traces-5M')
+    sample_output_dir = os.path.join(traces_dir, 'sample-output')
+    program = './build/release/project.out'
+
+    configs = [os.path.join(config_dir, c) for c in os.listdir(config_dir)]
+    traces = [os.path.join(traces_short_dir, t) for t in os.listdir(traces_short_dir)]
+    ztraces = [os.path.join(traces_5M_dir, 'sjeng.gz')]
+    traces.extend(ztraces)
+
+    for trace in traces:
+        for config in configs:
+            if trace in ztraces:
+                cat = 'zcat'
+                tracesep = '-5M.'
+            else:
+                cat = 'cat'
+                tracesep = '.'
+            trace_basename = os.path.splitext(os.path.basename(trace))[0]
+            config_basename = os.path.basename(config)
+
+            result = os.path.join(results_dir, trace_basename + '.' + config_basename)
+            good_result = os.path.join(sample_output_dir, trace_basename + tracesep + config_basename)
+
+            command_line = ' '.join([cat, trace, '|', program, config, '-t', trace_basename, '>', result])
+
+            os.system(command_line)
+
+            diff = ['diff', '-w', good_result, result]
+            try:
+                subprocess.check_output(diff)
+            except:
+                print 'invalid output for', os.path.basename(result)
+

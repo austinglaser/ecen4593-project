522de87c08b1582de49f49fefaabfbe110c8c08e
Austin Glaser <austin@boulderes.com>
2016-04-16 00:33:42 -0600

    document cexception configuration

--- inc/CExceptionConfig.h
+++ inc/CExceptionConfig.h
@@ -24,28 +24,37 @@
 
 /* --- PUBLIC CONSTANTS ----------------------------------------------------- */
 
-// TODO: Find a way not to have to do this
+/**@brief   An exception's type */
 #define CEXCEPTION_T volatile unsigned int
 
 /* --- PUBLIC DATATYPES ----------------------------------------------------- */
 /* --- PUBLIC MACROS -------------------------------------------------------- */
 
+/**@brief   Handler to execute if an exception is thrown outside of a Try {}
+ *          block
+ */
 #define CEXCEPTION_NO_CATCH_HANDLER(id) UncaughtException(id)
 
+/**@brief   Store some location information with a thrown exception */
 #define ThrowWithLocationInfo(e, file, line)    do {                       \
                                                     exception_file = file; \
                                                     exception_line = line; \
                                                     Throw(e);              \
                                                 } while (false)
+
+/**@brief   Store the current location with a thrown exception */
 #define ThrowHere(e) ThrowWithLocationInfo(e, __FILE__, __LINE__)
 
 /* --- PUBLIC VARIABLES ----------------------------------------------------- */
 
-extern const char * exception_file;
-extern unsigned int exception_line;
+extern const char * exception_file; /**< File that caused an exception */
+extern unsigned int exception_line; /**< Line that caused an exception */
 
 /* --- PUBLIC FUNCTIONS ----------------------------------------------------- */
 
+/**@brief   Handler to execute if an exception is thrown outside of a Try {}
+ *          block
+ */
 void UncaughtException(CEXCEPTION_T id);
 
 /** @} defgroup CEXCEPTIONCONFIG */
